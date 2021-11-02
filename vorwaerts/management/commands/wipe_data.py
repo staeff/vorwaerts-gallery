33816b7f4d2e4bb5ed96d02727f2022063a58404
Stephan Klinger <staeff@arcor.de>
2021-11-02 22:55:12 +0100

    Add advertising objects to custom wipe_date command

--- vorwaerts/management/commands/wipe_data.py
+++ vorwaerts/management/commands/wipe_data.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
 
 from vorwaerts.models import NewspaperPage
-
+from vorwaerts.models import ClassifiedAd
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         NewspaperPage.objects.all().delete()
+        ClassifiedAd.objects.all().delete()
